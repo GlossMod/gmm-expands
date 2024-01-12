41960ed7e027a970133ffd81d1a626a0663eead8
小莫 <xiaomo1433675239@outlook.com>
2024-01-12 17:41:24 +0800

    Update user authentication logic

---
+++ LethalCompany.ts
@@ -0,0 +1,32 @@
+/**
+ * @description 致命公司 支持
+ */
+
+import type { ISupportedGames } from "@src/model/Interfaces";
+import { join } from "node:path"
+import { UnityGame } from "@src/model/UnityGame";
+
+
+export const supportedGames: ISupportedGames = {
+    gameID: 329,
+    steamAppID: 1966720,
+    Thunderstore: {
+        community_identifier: 'lethal-company'
+    },
+    installdir: join("Lethal Company"),
+    gameName: "Lethal Company",
+    gameExe: "Lethal Company.exe",
+    startExe: [
+        {
+            name: "Steam 启动",
+            exePath: "steam://rungameid/1966720"
+        },
+        {
+            name: "直接启动",
+            exePath: join("Lethal Company.exe")
+        }
+    ],
+    gameCoverImg: "https://mod.3dmgame.com/static/upload/game/65a0f0fb13a40.webp",
+    modType: UnityGame.modType,
+    checkModType: UnityGame.checkModType
+}
