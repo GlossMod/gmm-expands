288e6693aa90712cb0a6e5240e248eb6f2905a80
小莫 <xiaomo1433675239@outlook.com>
2024-01-29 15:16:08 +0800

    更新 1.29.3

--- Palworld.ts
+++ Palworld.ts
@@ -6,6 +6,33 @@ import { join, extname, sep, basename, dirname } from 'path'
 import { Manager } from "@src/model/Manager";
 import { ElMessage } from "element-plus";
 import { FileHandler } from "@src/model/FileHandler";
+import { useManager } from "@src/stores/useManager";
+
+
+function setBPModLoaderMod() {
+
+    const manager = useManager()
+
+    let filepath = join(manager.gameStorage, "Pal", "Binaries", "Win64", "Mods", "mods.txt")
+
+    // 判断文件是否存在
+    if (!FileHandler.fileExists(filepath)) {
+        ElMessage.warning("未找到mods.txt, 部分Mod可能不生效, 请确保您已安装 UE4SS !")
+        return
+    }
+
+    // 读取文件
+    let data = FileHandler.readFile(filepath, "")
+    console.log(data);
+
+    // 将 BPModLoaderMod : 1 改为 0
+    data = data.replace(/BPModLoaderMod : 0/g, "BPModLoaderMod : 1")
+    console.log(data);
+
+    // 写入回去
+    FileHandler.writeFile(filepath, data)
+}
+
 
 export const supportedGames: ISupportedGames = {
     GlossGameId: 333,
@@ -28,8 +55,9 @@ export const supportedGames: ISupportedGames = {
         {
             id: 1,
             name: 'pak',
-            installPath: join('Pal', 'Content', 'Paks', '~mods'),
+            installPath: join('Pal', 'Content', 'Paks', 'LogicMods'),
             async install(mod) {
+                setBPModLoaderMod()
                 return Manager.generalInstall(mod, this.installPath ?? "")
             },
             async uninstall(mod) {
@@ -82,6 +110,48 @@ export const supportedGames: ISupportedGames = {
             }
         },
         {
+            id: 4,
+            name: "LogicMods",
+            installPath: join("Pal", "Content", "Paks", "LogicMods"),
+            async install(mod) {
+                setBPModLoaderMod()
+                return Manager.generalInstall(mod, this.installPath ?? "")
+            },
+            async uninstall(mod) {
+                return Manager.generalUninstall(mod, this.installPath ?? "")
+            }
+        },
+        {
+            id: 5,
+            name: "Scripts",
+            installPath: join("Pal", "Binaries", "Win64", "Mods"),
+            async install(mod) {
+                const manage = useManager()
+
+                // 获取 Scripts 文件夹的父级目录
+                let parent = ""
+                for (let index in mod.modFiles) {
+                    const element = mod.modFiles[index];
+                    let arr = FileHandler.pathToArray(element)
+                    if (arr.includes("Scripts")) {
+                        parent = dirname(element)
+                        break
+                    }
+                }
+
+                let enableFile = join(manage.modStorage, mod.id.toString(), parent, "Enabled.txt")
+
+                console.log(enableFile);
+                FileHandler.ensureDirectoryExistence(enableFile)
+
+
+                return Manager.installByFolderParent(mod, this.installPath ?? "", "Scripts", true)
+            },
+            async uninstall(mod) {
+                return Manager.installByFolderParent(mod, this.installPath ?? "", "Scripts", false)
+            }
+        },
+        {
             id: 99,
             name: '未知',
             installPath: '\\',
@@ -95,21 +165,28 @@ export const supportedGames: ISupportedGames = {
         }
     ],
     checkModType(mod) {
-
         let pak = false
         let us4ss = false
         let mods = false
+        let Scripts = false
+
         mod.modFiles.forEach(item => {
             if (extname(item) == '.pak') pak = true
             if (FileHandler.compareFileName(item, 'Enabled.txt')) mods = true
 
             if (FileHandler.compareFileName(item, 'ue4ss.dll')) us4ss = true
             if (FileHandler.compareFileName(item, 'xinput1_3.dll')) us4ss = true
+
+            // 路径中是否有 Scripts
+            let arr = FileHandler.pathToArray(item)
+            if (arr.includes("Scripts")) Scripts = true
+
         })
 
         if (us4ss) return 2
         if (pak) return 1
         if (mods) return 3
+        if (Scripts) return 5
 
         return 99
     }
