0eaf3524173c508fe2b1dd404ec6632136f5fb82
小莫 <xiaomo1433675239@outlook.com>
2024-04-30 10:09:48 +0800

    更新 1.39.1

---
+++ ManorLords.ts
@@ -0,0 +1,29 @@
+/**
+ * @description 庄园领主 支持
+ */
+import type { ISupportedGames } from "@src/model/Interfaces";
+import { join } from 'path'
+
+import { UnrealEngine } from "@src/model/UnrealEngine"
+
+
+export const supportedGames: ISupportedGames = {
+    GlossGameId: 348,
+    steamAppID: 1363080,
+    installdir: join("Manor Lords"),
+    gameName: "Manor Lords",
+    startExe: [
+        {
+            name: "Steam 启动",
+            exePath: "steam://rungameid/1363080"
+        },
+        {
+            name: "直接启动",
+            exePath: "ManorLords.exe"
+        }
+    ],
+    gameExe: "ManorLords.exe",
+    gameCoverImg: "https://mod.3dmgame.com/static/upload/game/662db2a8b4521.webp",
+    modType: UnrealEngine.modType("ManorLords", false),
+    checkModType: UnrealEngine.checkModType
+}
