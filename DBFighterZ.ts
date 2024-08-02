829a222c00187e4faf91c2f8ebdab1a4d1ec241f
小莫 <xiaomo1433675239@outlook.com>
2024-08-02 15:22:00 +0800

    更新 1.47.0

--- DBFighterZ.ts
+++ DBFighterZ.ts
@@ -4,23 +4,23 @@ import { FileHandler } from "@src/model/FileHandler";
 import { UnrealEngine } from "@src/model/UnrealEngine";
 
 export const supportedGames: ISupportedGames = {
-    GlossGameId: 207,
-    steamAppID: 678960,
-    gameName: "CODE VEIN",
-    installdir: "CodeVein",
-    gameExe: 'CodeVein.exe',
+    GlossGameId: 157,
+    steamAppID: 678950,
+    gameName: "DRAGON BALL FighterZ",
+    installdir: "DRAGON BALL FighterZ",
+    gameExe: 'DBFighterZ.exe',
     startExe: [
         {
             name: 'Steam 启动',
-            exePath: 'steam://rungameid/678960'
+            exePath: 'steam://rungameid/678950'
         },
         {
             name: '直接启动',
-            exePath: 'CodeVein.exe'
+            exePath: 'DBFighterZ.exe'
         }
     ],
     archivePath: join(FileHandler.GetAppData(), "Local", "CodeVein", "Saved"),
-    gameCoverImg: "https://mod.3dmgame.com/static/upload/game/207.png",
-    modType: UnrealEngine.modType("CodeVein", false),
+    gameCoverImg: "https://mod.3dmgame.com/static/upload/game/157.jpg",
+    modType: UnrealEngine.modType("RED", false),
     checkModType: UnrealEngine.checkModType
 }
